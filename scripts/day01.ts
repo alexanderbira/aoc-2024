ed1c9d1a1dedfc56c0e5c93ac98996f123fb0da2
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-12-01 05:13:09 +0000

    add day 1 solution

--- scripts/day01.ts
+++ scripts/day01.ts
@@ -1,4 +1,27 @@
-import read from "../read.ts";
-const input = await read(1);
+import read from "../read.ts"
 
-console.log(input);
+const input = await read(1)
+
+const locationIds = input.split("\n").map(row => row.split("   ").map(Number))
+
+const leftIds = locationIds.map(x => x[0])
+const rightIds = locationIds.map(x => x[1])
+
+leftIds.sort((a, b) => a - b)
+rightIds.sort((a, b) => a - b)
+
+let totalDistance = 0
+for (let i = 0; i < leftIds.length; i++) {
+  totalDistance += Math.abs(leftIds[i] - rightIds[i])
+}
+console.log(totalDistance)
+
+let similarityScore = 0
+for (const leftId of leftIds) {
+  let numLeftInRight = 0;
+  for (const rightId of rightIds) {
+    if (leftId === rightId) numLeftInRight++;
+  }
+  similarityScore += leftId * numLeftInRight
+}
+console.log(similarityScore)
